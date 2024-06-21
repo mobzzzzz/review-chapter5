232a202bf64095e13ceb400b1bfc01155d793d97
Jaewon Noh <opqw987@gmail.com>
2024-06-21 15:13:18 +0900

    :tada: Initialize Project

---
+++ src/main/kotlin/sparta/nbcamp/reviewchapter5/ReviewChapter5Application.kt
@@ -0,0 +1,11 @@
+package sparta.nbcamp.reviewchapter5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewChapter5Application
+
+fun main(args: Array<String>) {
+    runApplication<ReviewChapter5Application>(*args)
+}
---
+++ src/test/kotlin/sparta/nbcamp/reviewchapter5/ReviewChapter5ApplicationTests.kt
@@ -0,0 +1,12 @@
+package sparta.nbcamp.reviewchapter5
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReviewChapter5ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
