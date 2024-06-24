52e9d060db38016acba7083c9511f7520dd97b49
Jaewon Noh <opqw987@gmail.com>
2024-06-24 16:53:22 +0900

    :sparkles: 6/25 개선과제 Post Response 내용 추가

--- src/main/kotlin/sparta/nbcamp/reviewchapter5/domain/post/dto/response/PostResponse.kt
+++ src/main/kotlin/sparta/nbcamp/reviewchapter5/domain/post/dto/response/PostResponse.kt
@@ -4,15 +4,18 @@ import sparta.nbcamp.reviewchapter5.domain.post.dto.category.response.CategoryRe
 import sparta.nbcamp.reviewchapter5.domain.post.dto.tag.response.TagResponse
 import sparta.nbcamp.reviewchapter5.domain.post.model.Post
 import sparta.nbcamp.reviewchapter5.domain.user.dto.response.UserResponse
+import java.time.LocalDateTime
 
 data class PostResponse(
     val id: Long,
     val title: String,
     val content: String,
     val status: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
     val user: UserResponse,
     val category: CategoryResponse? = null,
-    val tagList: List<TagResponse> = emptyList()
+    val tagList: List<TagResponse> = emptyList(),
 ) {
     companion object {
         fun from(post: Post) = PostResponse(
@@ -20,6 +23,8 @@ data class PostResponse(
             title = post.title,
             content = post.content,
             status = post.status.name,
+            createdAt = post.createdAt,
+            updatedAt = post.updatedAt,
             user = UserResponse.from(post.user),
             category = post.category.let { CategoryResponse.from(it) },
             tagList = post.tagList.map { TagResponse.from(it) }
