4dc1f44f414f26b875c324631ad75907d8070fdb
Jaewon Noh <opqw987@gmail.com>
2024-06-28 15:10:11 +0900

    :heavy_plus_sign: springmockk 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ val queryDslVersion = "5.0.0"
 val mockkVersion = "1.13.9"
 val kotestVersion = "5.8.1"
 val fixtureMonkeyVersion = "1.0.14"
+val springMockkVersion = "4.0.2"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -60,6 +61,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("com.ninja-squad:springmockk:$springMockkVersion")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
